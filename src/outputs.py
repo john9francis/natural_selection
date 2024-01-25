4527f50305fd101fcb470af1d7d0f9551fbd79cf
John Francis <john9francis@gmail.com>
2024-01-24 22:55:37 -0700

    Added more outputs to go up and down

--- src/outputs.py
+++ src/outputs.py
@@ -4,16 +4,16 @@ from src import base_input_output
 
 class MoveLeft(base_input_output.Output):
   def trigger_output(self):
-    self.move_left()
-
-  def move_left(self):
     self.creature.pos.x -= 10
-
-
 
 class MoveRight(base_input_output.Output):
   def trigger_output(self):
-    self.move_right()
-  
-  def move_right(self):
     self.creature.pos.x += 10
+
+class MoveUp(base_input_output.Output):
+  def trigger_output(self):
+    self.creature.pos.y -= 10
+
+class MoveDown(base_input_output.Output):
+  def trigger_output(self):
+    self.creature.pos.y += 10
