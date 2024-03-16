de0ae68c6a5155e996a88997ddc63496b68e6d54
John Francis <john9francis@gmail.com>
2024-03-16 11:30:52 -0600

    Added a few more functions to the derived creatures

--- interface.py
+++ interface.py
@@ -22,13 +22,19 @@ class MyCreature(base_creature.BaseCreature):
     self.start_time = 0.
     self.time_interval = self.rand.uniform(100, 500)
 
-    self.pos = pygame.Vector2(self.rand.uniform(100, 500), 500)
+    self.pos = pygame.Vector2(self.rand.uniform(100, 500), self.rand.uniform(100, 500))
     self.size= pygame.Vector2(10, 10)
 
     self.speed = 100
 
     self.add_input_funcs([self.my_input_func])
-    self.add_output_funcs([self.move_left, self.move_right])
+    self.add_output_funcs([self.move_left, self.move_right, self.move_down, self.move_up])
+
+    # for staying on the screen
+    self.left_bound = 0
+    self.top_bound = 0
+    self.right_bound = 500
+    self.bottom_bound = 500
 
 
   def draw(self, screen):
@@ -37,6 +43,22 @@ class MyCreature(base_creature.BaseCreature):
   def update(self, dt):
     super().update(dt)
 
+    self.stay_on_screen()
+
+
+  def stay_on_screen(self):
+    if self.pos.x > self.right_bound:
+      self.pos.x = self.right_bound
+    if self.pos.x < self.left_bound:
+      self.pos.x = self.left_bound
+    if self.pos.y > self.bottom_bound:
+      self.pos.y = self.bottom_bound
+    if self.pos.y < self.top_bound:
+      self.pos.y = self.top_bound
+
+  def set_bounds(self, screen_width, screen_height):
+    self.right_bound = screen_width
+    self.bottom_bound = screen_height
 
   def my_input_func(self) -> bool:
     # Calculate elapsed time
@@ -52,11 +74,15 @@ class MyCreature(base_creature.BaseCreature):
 
   def move_left(self):
     self.pos.x -= self.speed
-    pass
 
   def move_right(self):
     self.pos.x += self.speed
-    pass
+
+  def move_up(self):
+    self.pos.y -= self.speed
+  
+  def move_down(self):
+    self.pos.y += self.speed
 
 
   
@@ -65,7 +91,17 @@ class MyCreature(base_creature.BaseCreature):
 def main():
   display = base_display.BaseDisplay()
 
-  display.add_creatures([MyCreature() for _ in range(10)])
+  c_list = []
+
+  screen_width, screen_height = display.get_screen_bounds()
+
+  for _ in range(10):
+    c = MyCreature()
+    c.set_bounds(screen_width, screen_height)
+    c_list.append(c)
+    
+
+  display.add_creatures(c_list)
 
   display.main()
 
--- natural_selection/base/base_display.py
+++ natural_selection/base/base_display.py
@@ -5,6 +5,7 @@ import pygame
 class BaseDisplay():
   def __init__(self) -> None:
     self.c_list = []
+    self.screen = pygame.display.set_mode((1280, 720))
     pass
 
 
@@ -13,12 +14,15 @@ class BaseDisplay():
     self.c_list.extend(new_creatures)
 
 
+  def get_screen_bounds(self):
+    return self.screen.get_width(), self.screen.get_height()
+
+
 
   def main(self):
 
     # initialization settings:
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
 
     dt = 0.
@@ -32,14 +36,14 @@ class BaseDisplay():
           running = False
 
       # re-draw the background each frame so we don't get previous frames showing up
-      screen.fill("black")
+      self.screen.fill("black")
 
       dt = clock.tick(60) / 1000.0
 
       # update our creatures
       for c in self.c_list:
         c.update(dt)
-        c.draw(screen)
+        c.draw(self.screen)
 
       # update the screen
       pygame.display.flip()
